cfbd3770b2e8d6e96b887294a932669673a0abcf
rafeseth <rafael.m.f.contato@hotmail.com>
2024-05-04 16:42:43 -0300

    commit com projetos

---
+++ GeekShopping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(int id);
+        Task<ProductVO> GetAll();
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(int id);
+    }
+}
---
+++ GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MySqlContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductRepository(MySqlContext mySqlContext, IMapper mapper)
+        {
+            _context = mySqlContext;
+            _mapper = mapper;
+        }
+        public async Task<ProductVO> Create(ProductVO vo)
+        {
+            Product product= _mapper.Map<Product>(vo);
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+
+        public async Task<ProductVO> Update(ProductVO vo)
+        {
+            Product product = _mapper.Map<Product>(vo);
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+        public async Task<bool> Delete(int id)
+        {
+            try
+            {
+                Product product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+                if (product != null)
+                {
+                    _context.Products.Remove(product);
+                    _context.SaveChangesAsync();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<ProductVO>> FindAll()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+            return _mapper.Map<List<ProductVO>>(products);
+        }
+
+        public async Task<ProductVO> FindById(int id)
+        {
+            Product product = await _context.Products.Where(p => p.Id==id).FirstOrDefaultAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+
+        public Task<ProductVO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
